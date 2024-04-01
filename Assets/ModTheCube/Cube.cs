50e2b1951f0194278a1676e9c2c4d647075149d4
Thunderball5892 <157115726+Thunderball5892@users.noreply.github.com>
2024-04-01 16:06:24 +1000

    Project Files
    Mod-The-Cube Project Files Import

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(10, 20), 4, 1);
+        transform.localScale = Vector3.one * 5.2f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(1, 1, 2, 3);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(20.0f * Time.deltaTime * Random.Range(2, 13), 0.0f, 0.0f);
+    }
+}
